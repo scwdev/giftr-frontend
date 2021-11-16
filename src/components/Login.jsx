33ea0b7613b2b51bc245b79999142af605e04e62
jmalabed <jmalabed27@gmail.com>
2021-11-15 20:19:57 -0800

    updated and set up basic pages

---
+++ src/components/Login.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import { Form, Button, Row, Col, Container, Card } from "react-bootstrap";
+
+const Login = (props) => {
+  // https://giftr-back.herokuapp.com/ back end link
+  const [input, setInput] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    logIn(input);
+  };
+  // not yet working log in. just threw in skeleton. perhaps axios call? I am not familiar. - jm
+  const logIn = async (data) => {
+    try {
+      const configs = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const loggedIn = await fetch(
+        "https://giftr-back.herokuapp.com/login",
+        configs
+      ); // check fetch url!!
+      const parsedLogIn = await loggedIn.json();
+      console.log(parsedLogIn);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <Container>
+        <Card>
+          <h1>Login</h1>
+          <Form
+            onSubmit={handleSubmit}
+            className="d-flex justify-content-center"
+          >
+            <div className="w-50">
+              <Form.Group className="">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
+                  type="text"
+                  id="username"
+                  name="username"
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  name="password"
+                  id="password"
+                  type="password"
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <Form.Control type="submit" />
+            </div>
+          </Form>
+        </Card>
+      </Container>
+    </div>
+  );
+};
+export default Login;
---
+++ src/config/Router.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../components/Home";
+import About from "../components/About";
+import Login from "../components/Login";
+import SignUp from "../components/SignUp";
+
+const Router = (props) => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<SignUp />} />
+    </Routes>
+  );
+};
+export default Router;

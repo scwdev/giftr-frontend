e2e1eb20c066b5b29ec555980b89d9016ca88a2e
jmalabed <jmalabed27@gmail.com>
2021-11-16 14:43:39 -0800

    update wishlist component

--- src/config/Router.jsx
+++ src/config/Router.jsx
@@ -3,6 +3,7 @@ import Home from "../components/Home";
 import About from "../components/About";
 import Login from "../components/Login";
 import SignUp from "../components/SignUp";
+import List from "../components/List";
 
 const Router = (props) => {
   return (
@@ -11,6 +12,7 @@ const Router = (props) => {
       <Route path="about" element={<About />} />
       <Route path="login" element={<Login />} />
       <Route path="signup" element={<SignUp />} />
+      <Route path="wishlist/:id" element={<List />} />
     </Routes>
   );
 };

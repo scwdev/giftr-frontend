2e6f8d63b5073124ffbb50317a322fbe7d6cb81b
jekkiboi <hollymayer4@gmail.com>
2021-11-16 15:49:04 -0800

    styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,24 @@
+
+
 const Home = (props) => {
   return (
-    <div>
-      <h1>Welcome</h1>
-      <p className="">Giftr is...</p>
+    <div className='home-index' style={{
+      backgroundImage: `url('${process.env.PUBLIC_URL}/images/bg1.png')`,
+      backgroundPosition: "bottom",
+      backgroundSize: "fill"
+    }}>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      <div className="title-line">
+      <h1 className="title">Giftr</h1> &nbsp; &nbsp;
+      <img className='giftrimg' src={process.env.PUBLIC_URL + '/images/Giftr2d.png'} alt=""/>
+      </div>
+      <p className="tagline">Giftr is...</p>
     </div>
   );
 };
